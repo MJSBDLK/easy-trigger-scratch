fc4ff79c3ed033db2a7107073ff4cc42a1a0ae80
MJSBDLK <rqdavismail@gmail.com>
2023-08-20 20:45:01 -0700

    re-committing w/ gitignore

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,273 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    #region Variables
+
+    [Header("Components")]
+    private Animator animator;
+    // private CapsuleCollider2D capsuleCollider;
+    private LayerMask playerLayerMask;
+    [SerializeField] private Transform playerSprite;
+    private Rigidbody2D rigidBody;
+
+    [Header("State Variables")]
+    private bool facingRight = true;
+    private bool crouching = false;
+    public float horizontalMovement; // -1 | 0 | 1
+    private bool jumpButtonDown;
+    private int jumpSquatCounter;
+    private Vector2 aimDirection;
+
+    [Header("Player Attributes")]
+    public float baseGravity;
+    public float baseJumpForce = 3;
+    [SerializeField] private int jumpSquatFrames = 4;
+    public float shortHopForce = 1.0f;
+    public float fullHopForce = 1.25f;
+    public float movementSpeed = 4.0f;
+    public float deadZone = 0.19f;
+    public float tiltRadius = 0.6f; // This shouldn't do anything with arrow keys
+
+    [Header("Grounded Check Variables")]
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundLayerMask;
+    [SerializeField] private LayerMask oneWayPlatformLayer;
+    [SerializeField] private LayerMask playerHurtBoxLayer;
+    private int playerLayerNumber;
+    private int playerHurtBoxLayerNumber;
+    // private int groundLayerNumber;
+    private int oneWayPlatformLayerNumber;
+    public bool playerIsGrounded;
+
+    [Header("Aim Rotation")]
+    public Transform spriteTransform;
+    public Transform rotationPivot;
+    private Quaternion originalRotation;
+    #endregion
+
+    private void Start()
+    {
+        #region Get Components
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        #endregion
+
+        #region Layer mask variable init
+        playerLayerMask = 1 << gameObject.layer;
+        playerLayerNumber = GetLayerFromMask(playerLayerMask);
+        playerHurtBoxLayerNumber = GetLayerFromMask(playerHurtBoxLayer);
+        // groundLayerNumber = GetLayerFromMask(groundLayerMask);
+        oneWayPlatformLayerNumber = GetLayerFromMask(oneWayPlatformLayer);
+        #endregion
+
+        baseGravity = rigidBody.gravityScale;
+        originalRotation = spriteTransform.rotation;
+    }
+
+    /* Poll for input every frame */
+    private void Update()        // Poll for input
+    {
+        #region Crouch Input
+        // Check for down on primary axis
+        float verticalAxisInput = Input.GetAxisRaw("LeftVertical");
+        // Debug.Log("verticalAxisInput " + verticalAxisInput);
+        if (verticalAxisInput < (-1 * tiltRadius)) // Don't want to detect minor tilts on control stick
+        {
+            if (playerIsGrounded)
+            {
+                Crouch();
+            }
+            else // if player is airborne
+            {
+                FastFall();
+            }
+        }
+        else // If not inputting down
+        {
+            UnCrouch();
+        }
+        #endregion
+
+        #region Horizontal Movement Input
+        if (!crouching) { horizontalMovement = Input.GetAxisRaw("LeftHorizontal"); }
+        #endregion
+
+        #region Jump Input
+        if (Input.GetButtonDown("Jump") && playerIsGrounded && jumpSquatCounter == 0)
+        {
+            jumpSquatCounter = jumpSquatFrames;
+            jumpButtonDown = true;
+        } // Jump();
+
+        // Detect a shorthop if button is released before end of jumpSquat
+        if (jumpButtonDown && Input.GetButtonUp("Jump") && jumpSquatCounter > 0)
+        {
+            jumpButtonDown = false;
+        }
+        #endregion
+
+        #region Aim Input
+        aimDirection = GetAimVector();
+        #endregion
+    }
+
+    /* Apply input at fixed interval */
+    private void FixedUpdate()
+    {
+        CheckGrounded();
+        HandleOneWayPlatformCollision();
+
+        #region Horizontal Movement
+        if (!crouching) rigidBody.velocity = new Vector2(horizontalMovement * movementSpeed, rigidBody.velocity.y);
+        CheckFlipHorizontal();
+        // animator.SetFloat("playerSpeed", Mathf.Abs(horizontalMovement));
+        animator.SetFloat("playerSpeed", Mathf.Abs(rigidBody.velocity.x));
+        #endregion
+
+        #region Jump
+        if (jumpSquatCounter > 0) // Stop counting down at 0
+        {
+            animator.SetBool("isCrouching", jumpSquatCounter > 0); // Using crouch sprite for jumpSquat anim
+
+            jumpSquatCounter--;
+
+            if (jumpSquatCounter == 0)
+            {
+                Jump();
+            }
+        }
+        #endregion
+
+        #region Air Aiming
+        if (!playerIsGrounded && aimDirection.magnitude > deadZone)
+        {
+            float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+            // Debug.Log("angle: " + angle);
+            if (!facingRight) // If player is not facing right, we should invert the aiming.
+            {
+                angle = -angle;
+            }
+            animator.transform.RotateAround(rotationPivot.position, Vector3.forward, angle - spriteTransform.rotation.eulerAngles.z);
+        }
+        else if (playerIsGrounded)
+        {
+            animator.transform.rotation = originalRotation;
+        }
+        #endregion
+    }
+
+    private void CheckFlipHorizontal() // Turn the player around
+    {
+        if (((horizontalMovement < 0) && facingRight) || ((horizontalMovement > 0) && !facingRight))
+        {
+            facingRight = !facingRight;
+            Vector3 playerScale = transform.localScale;
+            playerScale.x *= -1;
+            transform.localScale = playerScale;
+        }
+    }
+
+    private void CheckGrounded()
+    {
+        bool groundedOnGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundLayerMask);
+        bool groundedOnPlatform = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, oneWayPlatformLayer);
+
+        playerIsGrounded = (groundedOnGround || groundedOnPlatform);
+
+        if (playerIsGrounded)
+        {
+            rigidBody.gravityScale = baseGravity;
+        }
+        animator.SetBool("isGrounded", playerIsGrounded);
+    }
+
+    private void Crouch()
+    {
+        if (crouching) return; // don't want to run the rest of this code if we're already crouching
+        // Debug.Log("Crouch");
+        crouching = true;
+        animator.SetBool("isCrouching", true);
+    }
+
+    private void FastFall()
+    {
+        if (!playerIsGrounded)
+        {
+            rigidBody.gravityScale = baseGravity * 2;
+        }
+    }
+
+    private Vector2 GetAimVector()
+    {
+        float rightStickHorizontal = Input.GetAxis("RightHorizontal");
+        float rightStickVertical = Input.GetAxis("RightVertical");
+        Vector2 aimDir = new Vector2(rightStickHorizontal, rightStickVertical);
+        if (aimDir.magnitude > deadZone)
+        {
+            aimDir.Normalize();
+        }
+        else // We honestly don't need to be normalizing this, idk it just seems correct
+        {
+            aimDir = Vector2.zero;
+        }
+        return aimDir;
+    }
+    private int GetLayerFromMask(LayerMask mask)
+    {
+        int layerNumber = 0;
+        int maskValue = mask.value;
+        while (maskValue > 0)
+        {
+            maskValue = maskValue >> 1;
+            layerNumber++;
+        }
+        return layerNumber - 1;
+    }
+
+    private void HandleOneWayPlatformCollision()
+    {
+        // Always ignore collision between capsule collider and one-way platforms
+        Physics2D.IgnoreLayerCollision(playerHurtBoxLayerNumber, oneWayPlatformLayerNumber, true);
+
+        if (rigidBody.velocity.y <= 0)
+        {
+            Physics2D.IgnoreLayerCollision(playerLayerNumber, oneWayPlatformLayerNumber, false);
+        }
+        else
+        {
+            Physics2D.IgnoreLayerCollision(playerLayerNumber, oneWayPlatformLayerNumber, true);
+        }
+    }
+
+    private void Jump()
+    {
+        // Debug.Log("crouching: " + crouching);
+
+        float jumpMultiplier = jumpButtonDown ? fullHopForce : shortHopForce; // Jump higher if the jump button is still held.
+        if (crouching) jumpMultiplier *= 2;
+        float jumpForce = baseJumpForce * jumpMultiplier;
+
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        if (crouching) UnCrouch();
+        jumpButtonDown = false;
+        animator.SetBool("isCrouching", false);
+    }
+
+    private void UnCrouch()
+    { // We might integrate this with the animator so it gets its own function.
+        if (!crouching) return;
+        crouching = false;
+        animator.SetBool("isCrouching", false);
+    }
+
+    private void OnDrawGizmos() // For debugging
+    {
+        Gizmos.DrawSphere(groundCheckPoint.position, groundCheckRadius);
+    }
+}
