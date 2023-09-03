ebc626b0cacdbb82bf9e4c9ffc1b37aa4043f18b
MJSBDLK <rqdavismail@gmail.com>
2023-09-02 19:58:44 -0700

    flash on hit implemented

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,11 +5,16 @@ public class Health : MonoBehaviour
     public enum CharacterType { Player, Enemy }
     public CharacterType characterType;
     public int maxHealth = 100;
+    public Color damageFlashColor = Color.red; // Color to flash when taking damage
+    public float flashDuration = 0.1f; // Duration of the flash effect
+
     private int currentHealth;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         currentHealth = maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(int damageAmount, Vector2 hitDirection = default)
@@ -21,8 +26,23 @@ public class Health : MonoBehaviour
         }
         else
         {
-            // Damage feedback can be placed here, e.g., flash effect, damage sound
+            DamageFeedback();
         }
+    }
+
+    private void DamageFeedback()
+    {
+        // Audio here
+
+        StartCoroutine(FlashSprite());
+    }
+
+    private System.Collections.IEnumerator FlashSprite()
+    {
+        Color originalColor = spriteRenderer.color;
+        spriteRenderer.color = damageFlashColor;
+        yield return new WaitForSeconds(flashDuration);
+        spriteRenderer.color = originalColor;
     }
 
     private void Die(Vector2 hitDirection = default)
